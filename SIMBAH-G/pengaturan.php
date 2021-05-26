bb177e610bbed680f32d0fbcfcf4a5ef627af5cc
Chairul Imam <chairul_im@mhs.unsyiah.ac.id>
2021-05-26 21:44:08 +0700

    edit file pengaturan.php dan add meta tags

--- SIMBAH-G/pengaturan.php
+++ SIMBAH-G/pengaturan.php
@@ -2,27 +2,7 @@
 <html lang="en">
   <head>
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-    <!-- Fontawesome 5.15.3 -->
-    <link rel="stylesheet" href="fontawesome/css/all.min.css">
-
-    <!-- Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-
-    <!-- Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-
-    <!-- Custom CSS -->
-    <link href="css/sidebar.css" rel="stylesheet">
+    <?php include 'head-tags.php'; ?>
 
     <title>Halaman Pengaturan | SIMBAH-G</title>
 
