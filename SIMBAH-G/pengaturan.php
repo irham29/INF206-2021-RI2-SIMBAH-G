7b441187a00c6cf3bde212268ad3e8b6faafae30
Chairul Imam <chairul_im@mhs.unsyiah.ac.id>
2021-05-26 21:49:09 +0700

    include sidebar dan add sidebar.php

--- SIMBAH-G/pengaturan.php
+++ SIMBAH-G/pengaturan.php
@@ -9,19 +9,7 @@
   </head>
   <body>
 
-    <!-- Awal Navbar -->
-    <nav class="nav flex-column">
-      <h2>SIMBAH-G</h2><br>
-      <a class="nav-link akun" aria-current="page" href="akun.html">Akun</a><br>
-      <a class="nav-link beranda" href="beranda.html">Beranda</a>
-      <a class="nav-link kebutuhan" href="kebutuhan.html">Kebutuhan Gampong</a>
-      <a class="nav-link history" href="history.html">History</a>
-      <a class="nav-link hibah" href="berikanhibah.html">Berikan Hibah</a>
-      <a class="nav-link pengaturan" href="pengaturan.html">Pengaturan</a>
-      <a class="nav-link support" href="support.html">Support</a><br>
-      <a class="nav-link keluar" href="login.html">Keluar</a>
-    </nav>
-    <!-- Akhir Navbar -->
+    <?php include 'sidebar.php' ?>
     
     <!-- Modal Tambah -->
     <div class="modal fade" id="modal_tambah">
