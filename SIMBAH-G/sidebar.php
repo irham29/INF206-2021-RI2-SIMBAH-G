7651f610b1be7330987a251aa0532b9729f9802a
Chairul Imam <chairul_im@mhs.unsyiah.ac.id>
2021-05-26 21:59:35 +0700

    edit ekstensi file di href item navbar

--- SIMBAH-G/sidebar.php
+++ SIMBAH-G/sidebar.php
@@ -1,13 +1,13 @@
 <!-- Awal Navbar -->
 <nav class="nav flex-column">
   <h2>SIMBAH-G</h2><br>
-  <a class="nav-link akun" aria-current="page" href="akun.html">Akun</a><br>
-  <a class="nav-link beranda" href="beranda.html">Beranda</a>
-  <a class="nav-link kebutuhan" href="kebutuhan.html">Kebutuhan Gampong</a>
-  <a class="nav-link history" href="history.html">History</a>
-  <a class="nav-link hibah" href="berikanhibah.html">Berikan Hibah</a>
-  <a class="nav-link pengaturan" href="pengaturan.html">Pengaturan</a>
-  <a class="nav-link support" href="support.html">Support</a><br>
-  <a class="nav-link keluar" href="login.html">Keluar</a>
+  <a class="nav-link akun" aria-current="page" href="akun.php">Akun</a><br>
+  <a class="nav-link beranda" href="beranda.php">Beranda</a>
+  <a class="nav-link kebutuhan" href="kebutuhan.php">Kebutuhan Gampong</a>
+  <a class="nav-link history" href="history.php">History</a>
+  <a class="nav-link hibah" href="berikanhibah.php">Berikan Hibah</a>
+  <a class="nav-link pengaturan" href="pengaturan.php">Pengaturan</a>
+  <a class="nav-link support" href="support.php">Support</a><br>
+  <a class="nav-link keluar" href="login.php">Keluar</a>
 </nav>
 <!-- Akhir Navbar -->
