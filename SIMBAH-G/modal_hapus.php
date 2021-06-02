f01561eed8792f9797b1072a88612f15aaa53283
Chairul Imam <chairul_im@mhs.unsyiah.ac.id>
2021-06-02 18:45:06 +0700

    pindahkan modal_hapus ke file terpisah

---
+++ SIMBAH-G/modal_hapus.php
@@ -0,0 +1,25 @@
+<!-- Modal Hapus -->
+<div class="modal fade" id="modal_hapus">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+    
+      <!-- Header Modal Hapus-->
+      <div class="modal-header">
+        <h4 class="modal-title">Hapus Barang</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      
+      <!-- Body Modal Hapus-->
+      <div class="modal-body">
+        Apakah anda yakin ingin menghapus barang ini?
+      </div>
+
+      <!-- Footer Modal Hapus-->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+        <a href="hapus.php?id=" class="btn btn-success">Hapus</a>
+      </div>
+
+    </div>
+  </div>
+</div>
--- SIMBAH-G/pengaturan.php
+++ SIMBAH-G/pengaturan.php
@@ -15,32 +15,6 @@
     
     <?php include 'modal_tambah.php'; ?>
 
-    <!-- Modal Hapus -->
-    <div class="modal fade" id="modal_hapus">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-        
-          <!-- Header Modal Hapus-->
-          <div class="modal-header">
-            <h4 class="modal-title">Hapus Barang</h4>
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-          </div>
-          
-          <!-- Body Modal Hapus-->
-          <div class="modal-body">
-            Apakah anda yakin ingin menghapus barang ini?
-          </div>
-
-          <!-- Footer Modal Hapus-->
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
-            <button type="button" class="btn btn-success">Hapus</button>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
     <div class="isi">
       <div class="row">
         <h1>Pengaturan</h1>
@@ -88,6 +62,7 @@
 
               <?php 
                 include 'modal_edit.php'; 
+                include 'modal_hapus.php';
                 }
               ?>
             
