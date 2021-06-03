b41a6d728287a3f17fc5c7a106aa9934138dbea3
Chairul Imam <chairul_im@mhs.unsyiah.ac.id>
2021-06-03 08:33:51 +0700

    merapikan indentasi modal_edit

--- SIMBAH-G/modal_edit.php
+++ SIMBAH-G/modal_edit.php
@@ -28,11 +28,12 @@
         <div class="form-group">
           <div class="row">
           <label class="col-sm-3 control-label text-right font-weight-bold">Kategori</label>
-            <div class="col-sm-8"><select name="edit_kategori" class="form-control select2" style="width: 100 %;">
-              <option value="Pilih Kategori" >Pilih Kategori</option>
-              <option value="Sarpras" <?php echo $barang['kategori'] == 'Sarpras'? 'selected' : ''; ?>>Sarpras</option>
-              <option value="Meunasah" <?php echo $barang['kategori'] == 'Meunasah'? 'selected' : ''; ?>>Meunasah</option>
-            </select>
+            <div class="col-sm-8">
+              <select name="edit_kategori" class="form-control select2" style="width: 100 %;">
+                <option value="Pilih Kategori" >Pilih Kategori</option>
+                <option value="Sarpras" <?php echo $barang['kategori'] == 'Sarpras'? 'selected' : ''; ?>>Sarpras</option>
+                <option value="Meunasah" <?php echo $barang['kategori'] == 'Meunasah'? 'selected' : ''; ?>>Meunasah</option>
+              </select>
             </div>
           </div>
         </div>
@@ -41,7 +42,9 @@
         <div class="form-group">
           <div class="row">
             <label class="col-sm-3 control-label text-right font-weight-bold">Nama Barang</span></label>         
-          <div class="col-sm-8"><input type="text" class="form-control" name="edit_nama" value="<?php echo $barang['nama']; ?>"></div>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" name="edit_nama" value="<?php echo $barang['nama']; ?>">
+            </div>
           </div>
         </div>
 
@@ -49,15 +52,19 @@
         <div class="form-group">
           <div class="row">
             <label class="col-sm-3 control-label text-right font-weight-bold">Target</label>
-          <div class="col-sm-8"><input type="number" class="form-control" name="edit_target" value="<?php echo $barang['target']; ?>"></div>
+            <div class="col-sm-8">
+              <input type="number" class="form-control" name="edit_target" value="<?php echo $barang['target']; ?>">
+            </div>
           </div>
         </div>
         
         <!-- Tercapai -->
-          <div class="form-group">
+        <div class="form-group">
           <div class="row">
             <label class="col-sm-3 control-label text-right font-weight-bold">Tercapai</label>
-          <div class="col-sm-8"><input type="number" class="form-control" name="edit_tercapai" value="<?php echo $barang['tercapai']; ?>"></div>
+            <div class="col-sm-8">
+              <input disabled type="number" class="form-control" name="edit_tercapai" value="<?php echo $barang['tercapai']; ?>">
+            </div>
           </div>
         </div>
         
